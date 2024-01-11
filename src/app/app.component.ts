1eb3c32bfb0410f69eec7df7963b30b674e78d58
arrrnold <tm02arnold@gmail.com>
2024-01-10 21:51:33 -0600

    Plantilla aplicada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {AsideComponent} from "./aside/aside.component";
+import {FooterComponent} from "./footer/footer.component";
+import {BodyComponent} from "./body/body.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AsideComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
