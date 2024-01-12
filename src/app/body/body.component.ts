324d86825979fc2aa01f57f46d5e82ad513e01fd
arrrnold <tm02arnold@gmail.com>
2024-01-11 18:39:13 -0600

    se muestran las categorias

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
+
+  categorias: any[] = [
+    {id: 20, nombre: "Alimentos", descripcion: "Alimentos para toda ocasión"},
+    {id: 21, nombre: "Bebidas", descripcion: "Bebidas para toda ocasión"},
+    {id: 22, nombre: "Hogar", descripcion: "Productos para el hogar"},
+    {id: 23, nombre: "Limpieza", descripcion: "Productos de limpieza"},
+    {id: 24, nombre: "Mascotas", descripcion: "Productos para mascotas"},
+    {id: 25, nombre: "Salud", descripcion: "Productos para la salud"},
+    {id: 26, nombre: "Tecnología", descripcion: "Productos tecnológicos"},
+    {id: 27, nombre: "Vestuario", descripcion: "Productos de vestuario"}
+  ];
 
 }
